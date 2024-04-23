cb744aa89124044c8da637ff40aff9cebfe70281
Pisotski <vlad.pisotski@gmail.com>
2024-04-22 18:28:10 -0700

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
@@ -29,12 +34,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
-
 // routes
 app.get("/", (req, res) => {
-	res.send("jobs api");
+	res.send(`<h1> movies API</h1><a href ="/api-docs">Documentation</a>`);
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/movies", authMiddleware, moviesRouter);
 app.use("/api/v1/auth", authRouter);
